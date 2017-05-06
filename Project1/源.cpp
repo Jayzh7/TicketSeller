b90a7951a454b316c4498e0c68007f9da841e6fe
Jayzh7 <jayzh7@hotmail.com>
2017-05-06 15:22:39 +0800

    semaphore created!

--- Project1/源.cpp
+++ Project1/源.cpp
@@ -4,6 +4,7 @@
 #define MAX_CUSTOMER_NUM 1000
 #define MAX_ADMIN_NUM 10
 #define MAX_MOVIE_NUM 6
+#define MAX_PLAY_TIME 10
 using namespace std;
 
 HANDLE cusHandle[MAX_CUSTOMER_NUM];
@@ -12,11 +13,21 @@ int cusNum = 0;
 int admNum = 0;
 
 typedef struct movieinfo {
-	SYSTEMTIME startTime;
-	SYSTEMTIME endTime;
+	SYSTEMTIME startTime[MAX_PLAY_TIME];
+	SYSTEMTIME endTime[MAX_PLAY_TIME];
 	int duration; // minutes
+	int tickets;  //remaining tickets 
 	string name;
 }movieInfo[10];
+
+void createCustomer();
+void createAdmin();
+void admin();
+void customer();
+void orderTickets(string );
+void initMovie(movieinfo *mi, SYSTEMTIME startTime, int duration, int tickets, string name);
+void initMovieList(movieInfo*);
+
 
 void createCustomer() {
 	TCHAR szFilename[MAX_PATH];
@@ -87,14 +98,62 @@ void createAdmin() {
 void admin(int ID) {
 	cout << "Please enter the password:" << endl;
 	//TODO check password
+	cout << "Dear Admin, Welcome!" << endl;
+	cout << "What do you wanna do?" << endl;
+	cout << "1: check out remaining tickets" << endl;
+	cout << "2: check out customers' advice" << endl;
+
 
 }
 
 void customer(int ID) {
+	int choice;
 	cout << "1: order tickets" << endl;
 	cout << "2: check out available movies" << endl;
+	cout << "Please input your choice:";
+	cin >> choice;
+	if (choice == 1) {
+		orderTickets("Roman Holiday");
+		getchar();
+	}
 }
 
+void orderTickets(string name){
+	HANDLE mutex = OpenSemaphore(SEMAPHORE_ALL_ACCESS, NULL, "mutex");
+	HANDLE movie = OpenSemaphore(SEMAPHORE_ALL_ACCESS, NULL, name.c_str());
+
+	cout << "Waiting in line .." << endl;
+	Sleep(1000);
+	WaitForSingleObject(mutex, INFINITE);
+	cout << "Buying ticket(s).." << endl;
+	Sleep(1000);
+	DWORD returnStatus = WaitForSingleObject(movie, 1);
+	if (returnStatus == WAIT_TIMEOUT) {
+		cout << "Sorry, tickets are all sold out!" << endl;
+	}
+	else if (returnStatus == WAIT_OBJECT_0) {
+		cout << "Congrats! You bought one ticket successfully" << endl;
+	}
+
+	ReleaseSemaphore(mutex, 1, NULL);
+
+	system("pause");
+}
+
+void initMovieList(movieInfo* mi) {
+	SYSTEMTIME st;
+	GetSystemTime(&st);
+	initMovie(mi[0], st, 90, 3, "Roman Holiday");
+	initMovie(mi[1], st, 80, 100, "Gone with wind");
+	//init more movies
+}
+
+void initMovie(movieinfo *mi, SYSTEMTIME startTime, int duration,int tickets, string name) {
+	mi->duration = duration;
+	mi->name = name;
+	mi->tickets = tickets;
+	CreateSemaphore(NULL, tickets, tickets, name.c_str());
+}
 int main(int argc, char** argv) {
 
 	if (argc > 1 && strcmp(argv[1], "customer") == 0) {
@@ -126,10 +185,13 @@ int main(int argc, char** argv) {
 
 		//initialize movie list
 		movieInfo *mi = reinterpret_cast<movieInfo*>(pData);
-		for (int i = 0; i < MAX_MOVIE_NUM; i++) {
-			mi[i]->duration = 0;
-		}
 
+		initMovieList(mi);
+		
+		UnmapViewOfFile(pData);
+
+		//create mutex name "mutex"
+		CreateSemaphore(NULL, 1, 1, "mutex");
 		
 		int id;
 		cout << "*********************************" << endl;
